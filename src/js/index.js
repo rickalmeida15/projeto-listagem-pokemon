8256a3f0107288e1cbf052955572d57ac90d6326
Henrique Almeida <ricknuvem1@gmail.com>
2023-05-30 16:43:03 -0300

    Erro anterior na mesclagem

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 const body = document.querySelector("body");
 const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
@@ -14,38 +13,3 @@ botaoAlterarTema.addEventListener("click", () => {
     imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
   }
 });
-=======
-<<<<<<< HEAD
-const botaoAlterarTema = document.getElementById("botao-alterar-tema");
-const body = document.querySelector("body");
-const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
-
-botaoAlterarTema.addEventListener("click", () => {
-  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
-
-  body.classList.toggle("modo-escuro");
-
-  if (modoEscuroEstaAtivo) {
-    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
-  } else {
-    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
-  }
-});
-=======
-const botaoAlterarTema = document.getElementById("botao-alterar-tema");
-const body = document.querySelector("body");
-const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
-
-botaoAlterarTema.addEventListener("click", () => {
-  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
-
-  body.classList.toggle("modo-escuro");
-
-  if (modoEscuroEstaAtivo) {
-    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
-  } else {
-    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
-  }
-});
->>>>>>> 58fd7f3ea23ca7e77fda182d46894e7d71c02d13
->>>>>>> 7b1fdeee0c9e2d2c0e8a8a7af44512a3b13aeb49
